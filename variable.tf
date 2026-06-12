1eb620df17e5981497e2126149782cc9855604d4
Rohit Jain <jainrrohit1212@gmail.com>
2026-06-13 00:26:04 +0530

    Added Terraform code

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "resource_group" {}
+
+variable "virtual_network" {}
+
+variable "subnet" {}
+
+variable "network_interface" {}
+
+variable "network_security_group" {}
+
+variable "virtual_machine" {}
